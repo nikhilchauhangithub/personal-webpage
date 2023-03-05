134db5871a69b50fe2d745eee440104e82a2bdfa
thisisnikhilchauhan <thisisnikhilchauhan@gmail.com>
2023-03-05 14:10:25 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,488 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Nikhil - OnePage Portfolio</title>
+<link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
+
+<link rel="preconnect" href="https://fonts.googleapis.com/">
+<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="../../../maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="assets/css/animate.min.css">
+<link rel="stylesheet" href="assets/css/smooth-scrollbar.css">
+<link rel="stylesheet" href="assets/css/lightbox.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/responsive.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+
+<span class="icon-menu">
+<span class="bar"></span>
+<span class="bar"></span>
+</span>
+<div class="global-color">
+<span class="setting-toggle">
+<i class="las la-cog"></i>
+</span>
+<div class="inner">
+<div class="overlay"></div>
+<div class="global-color-option">
+<span class="close-settings">
+<i class="las la-times"></i>
+</span>
+<h2>Configuration</h2>
+<div class="global-color-option-inner">
+<p>Colors</p>
+<div class="color-boxed">
+<a href="#" class="clr-active" onclick="if (!window.__cfRLUnblockHandlers) return false; color1();" data-cf-modified-8144c0b248dad33d84b9b88a-=""></a>
+<a href="#" onclick="if (!window.__cfRLUnblockHandlers) return false; color2();" data-cf-modified-8144c0b248dad33d84b9b88a-=""></a>
+<a href="#" onclick="if (!window.__cfRLUnblockHandlers) return false; color3();" data-cf-modified-8144c0b248dad33d84b9b88a-=""></a>
+<a href="#" onclick="if (!window.__cfRLUnblockHandlers) return false; color4();" data-cf-modified-8144c0b248dad33d84b9b88a-=""></a>
+<a href="#" onclick="if (!window.__cfRLUnblockHandlers) return false; color5();" data-cf-modified-8144c0b248dad33d84b9b88a-=""></a>
+<a href="#" onclick="if (!window.__cfRLUnblockHandlers) return false; color6();" data-cf-modified-8144c0b248dad33d84b9b88a-=""></a>
+<a href="#" onclick="if (!window.__cfRLUnblockHandlers) return false; color7();" data-cf-modified-8144c0b248dad33d84b9b88a-=""></a>
+<a href="#" onclick="if (!window.__cfRLUnblockHandlers) return false; color8();" data-cf-modified-8144c0b248dad33d84b9b88a-=""></a>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+<div class="responsive-sidebar-menu">
+<div class="overlay"></div>
+<div class="sidebar-menu-inner">
+<div class="menu-wrap">
+<p>Menu</p>
+<ul class="menu scroll-nav-responsive d-flex">
+<li>
+<a class="scroll-to" href="#home">
+<i class="las la-home"></i> <span>Home</span>
+</a>
+</li>
+<li>
+<a class="scroll-to" href="#about">
+<i class="lar la-user"></i> <span>About</span>
+</a>
+</li>
+<li>
+<a class="scroll-to" href="#resume">
+<i class="las la-briefcase"></i> <span>Resume</span>
+ </a>
+</li>
+<li>
+<a class="scroll-to" href="#services">
+<i class="las la-stream"></i> <span>Services</span>
+</a>
+</li>
+<li>
+<a class="scroll-to" href="#skills">
+<i class="las la-shapes"></i> <span>Skills</span>
+</a>
+</li>
+<li>
+<a class="scroll-to" href="#portfolio">
+<i class="las la-grip-vertical"></i> <span>Portfolios</span>
+</a>
+</li>
+
+
+</ul>
+</div>
+<div class="sidebar-social">
+<p>Social</p>
+<ul class="social-links d-flex align-items-center">
+<li>
+<a href="#"><i class="lab la-twitter"></i></a>
+</li>
+<li>
+<a href="#"><i class="fa-brands fa-github"></i></a>
+</li>
+<li>
+<a href="#"><i class="fa-brands fa-linkedin"></i></a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+<ul class="menu scroll-nav d-flex">
+<li>
+<a class="scroll-to" href="#home">
+<span>Home</span> <i class="las la-home"></i>
+</a>
+</li>
+<li>
+<a class="scroll-to" href="#about">
+<span>About</span> <i class="lar la-user"></i>
+</a>
+</li>
+<li>
+<a class="scroll-to" href="#resume">
+<span>Resume</span> <i class="las la-briefcase"></i>
+</a>
+</li>
+<li>
+<a class="scroll-to" href="#services">
+<span>Services</span> <i class="las la-stream"></i>
+</a>
+</li>
+<li>
+<a class="scroll-to" href="#skills">
+<span>Skills</span> <i class="las la-shapes"></i>
+</a>
+</li>
+<li>
+<a class="scroll-to" href="#portfolio">
+<span>Portfolios</span> <i class="las la-grip-vertical"></i>
+</a>
+</li>
+<li>
+<a class="scroll-to" href="#testimonial">
+<span>Testimonial</span> <i class="lar la-comment"></i>
+</a>
+</li>
+<li>
+<a class="scroll-to" href="#contact">
+<span>Contact</span> <i class="las la-envelope"></i>
+</a>
+</li>
+</ul>
+<div class="left-sidebar">
+<div class="sidebar-header d-flex align-items-center justify-content-between">
+<h2>Nikhil</h2>
+<span class="designation">Framer Designer & Developer</span>
+</div>
+<img class="me" src="assets/images/nikhilprofile.png" alt="Me">
+
+<h2 class="address">Base in India</h2>
+<p class="copyright">&copy; 2022 Nikhil. All Rights Reserved</p>
+<ul class="social-profile d-flex align-items-center flex-wrap justify-content-center">
+<li>
+<a href="https://twitter.com/nikhilchauhantt"><i class="fa-brands fa-twitter"></i></a>
+</li>
+<li>
+<a href="https://www.cloudskillsboost.google/public_profiles/da800775-d745-4546-92b8-b8c63f900d12"><i class="fa-solid fa-cloud"></i></a>
+</li>
+<li>
+<a href="https://www.linkedin.com/in/nikhilchauhanldn/"><i class="fa-brands fa-linkedin"></i></a>
+</li>
+<li>
+<a href="https://github.com/nikhilchauhangithub"><i class="fa-brands fa-github"></i></a>
+</li>
+</ul>
+<a href="mailto:thisisnikhilchauhan?subject = Feedback&body = Message" class="theme-btn">
+<i class="las la-envelope"></i> Contact Me!
+</a>
+</div>
+<main class="drake-main">
+<div id="smooth-wrapper">
+<div id="smooth-content">
+<div class="left-sidebar">
+<div class="sidebar-header d-flex align-items-center justify-content-between">
+<h2>Nikhil</h2>
+<span class="designation">Web Developer</span>
+</div>
+<img class="me" src="assets/images/me.jpg" alt="Me">
+
+<h2 class="address">Base in India</h2>
+<p class="copyright">&copy; 2023 Nikhil. All Rights Reserved</p>
+<ul class="social-profile d-flex align-items-center flex-wrap justify-content-center">
+<li>
+<a href="#"><i class="lab la-twitter"></i></a>
+</li>
+<li>
+<a href="#"><i class="lab la-dribbble"></i></a>
+</li>
+<li>
+<a href="#"><i class="lab la-instagram"></i></a>
+</li>
+<li>
+<a href="#"><i class="lab la-github"></i></a>
+</li>
+</ul>
+<a href="mailto:thisisnikhilchauhan?subject = Feedback&body = Message" class="theme-btn">
+<i class="las la-envelope"></i> Contact Me!
+</a>
+</div>
+<section class="hero-section page-section scroll-to-page" id="home">
+<div class="custom-container">
+<div class="hero-content content-width">
+<div class="section-header">
+<h4 class="subtitle scroll-animation" data-animation="fade_from_bottom">
+<i class="las la-home"></i> Introduce
+</h4>
+<h1 class="scroll-animation" data-animation="fade_from_bottom">Hi from <span>Nikhil</span>, Web Designer and Developer</h1>
+</div>
+<p class="scroll-animation" data-animation="fade_from_bottom">I design and code beautifully simple things and i love what i do. Just simple like that!</p>
+<a href="#portfolio" class="go-to-project-btn scroll-to scroll-animation" data-animation="rotate_up">
+<img src="assets/images/round-text.png" alt="Rounded Text">
+<i class="las la-arrow-down"></i>
+</a>
+<div class="facts d-flex">
+<div class="left scroll-animation" data-animation="fade_from_left">
+<h1>2+</h1>
+<p>Years of <br>Experience</p>
+</div>
+<div class="right scroll-animation" data-animation="fade_from_right">
+<h1>5+</h1>
+<p>projects completed on</p>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section class="about-area page-section scroll-to-page" id="about">
+<div class="custom-container">
+<div class="about-content content-width">
+<div class="section-header">
+<h4 class="subtitle scroll-animation" data-animation="fade_from_bottom">
+<i class="lar la-user"></i> About
+</h4>
+<h1 class="scroll-animation" data-animation="fade_from_bottom">Every great design begin with<br>
+an even <span>better story</span></h1>
+</div>
+<p class="scroll-animation" data-animation="fade_from_bottom">Since beginning my journey as a freelance designer nearly 2 years ago,
+I've done remote work for agencies, and collaborated with
+talented people to create digital products for both business and consumer use.
+I'm quietly confident, naturally curious, and perpetually working on improving my
+chopsone design problem at a time.</p>
+</div>
+</div>
+</section>
+<section class="resume-area page-section scroll-to-page" id="resume">
+<div class="custom-container">
+<div class="resume-content content-width">
+<div class="section-header">
+<h4 class="subtitle scroll-animation" data-animation="fade_from_bottom">
+<i class="las la-briefcase"></i> Resume
+</h4>
+<h1 class="scroll-animation" data-animation="fade_from_bottom">Education & <span>Experience</span></h1>
+</div>
+<div class="resume-timeline">
+<div class="item scroll-animation" data-animation="fade_from_right">
+<span class="date">2023 - Present</span>
+<h2>Cloud</h2>
+<h2>Blockchain</h2>
+<h2>Exploring AI</h2>
+</div>
+<div class="item scroll-animation" data-animation="fade_from_right">
+<span class="date">2022</span>
+<h2>WebDeveloper Developer & Co-Founder</h2>
+<p>E-trainerapp</p>
+<h2>Php</h2>
+<h2>MySQl</h2>
+<p>Backend</p>
+<h2>SEO</h2>
+
+</div>
+<div class="item scroll-animation" data-animation="fade_from_right">
+<span class="date">2021</span>
+<h2>Ramanand Institute of Pharmacy MAnagement And Technology</h2>
+<p>College</p>
+<h2>Internsip as Reactjs Developer</h2>
+<p>Riddhika Developers</p>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section class="services-area page-section scroll-to-page" id="services">
+<div class="custom-container">
+<div class="services-content content-width">
+<div class="section-header">
+<h4 class="subtitle scroll-animation" data-animation="fade_from_bottom">
+<i class="las la-stream"></i> Services
+</h4>
+<h1 class="scroll-animation" data-animation="fade_from_bottom">My <span>Specializations</span></h1>
+</div>
+<div class="services-items">
+<div class="service-item scroll-animation" data-animation="fade_from_bottom">
+<i class="las la-bezier-curve"></i>
+<h2>Website Design</h2>
+<p>I created website with unique ideas use Figma & Html</p>
+<span class="projects">6+ Websites</span>
+</div>
+<div class="service-item scroll-animation" data-animation="fade_from_bottom">
+<i class="las la-code"></i>
+<h2>Development</h2>
+<p>I build webapp with Js and Php MySQl</p>
+<p>Build React Clones</p>
+<span class="projects">2 Projects</span>
+</div>
+<div class="service-item scroll-animation" data-animation="fade_from_bottom">
+<i class="las la-bezier-curve"></i>
+<h2>SEO/Marketing</h2>
+<p>Increase the traffic for your website with SEO optimized</p>
+<span class="projects">3 Projects</span>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section class="skills-area page-section scroll-to-page" id="skills">
+<div class="custom-container">
+<div class="skills-content content-width">
+<div class="section-header">
+<h4 class="subtitle scroll-animation" data-animation="fade_from_bottom">
+<i class="las la-shapes"></i> my skills
+</h4>
+<h1 class="scroll-animation" data-animation="fade_from_bottom">My <span>Advantages</span></h1>
+</div>
+<div class="row skills text-center">
+<div class="col-md-3 scroll-animation" data-animation="fade_from_left">
+<div class="skill">
+<div class="skill-inner">
+<img src="assets/images/figma.png" alt="Figma">
+<h1 class="percent">92%</h1>
+</div>
+<p class="name">HTML</p>
+</div>
+</div>
+<div class="col-md-3 scroll-animation" data-animation="fade_from_bottom">
+<div class="skill">
+<div class="skill-inner">
+<img src="assets/images/framer.png" alt="Framer">
+<h1 class="percent">85%</h1>
+</div>
+<p class="name">CSS</p>
+</div>
+</div>
+<div class="col-md-3 scroll-animation" data-animation="fade_from_top">
+<div class="skill">
+<div class="skill-inner">
+<img src="assets/images/webflow.png" alt="Webflow">
+<h1 class="percent">80%</h1>
+</div>
+<p class="name">JavaScript</p>
+</div>
+</div>
+<div class="col-md-3 scroll-animation" data-animation="fade_from_right">
+<div class="skill">
+ <div class="skill-inner">
+<img src="assets/images/react.png" alt="React">
+<h1 class="percent">90%</h1>
+</div>
+<p class="name">React</p>
+</div>
+</div>
+<div class="col-md-3 scroll-animation" data-animation="fade_from_left">
+<div class="skill">
+<div class="skill-inner">
+<img src="assets/images/wordpress.png" alt="WordPress">
+<h1 class="percent">86%</h1>
+</div>
+<p class="name">MySql</p>
+</div>
+</div>
+<div class="col-md-3 scroll-animation" data-animation="fade_from_bottom">
+<div class="skill">
+<div class="skill-inner">
+<img src="assets/images/laravel.png" alt="Laravel/PHP">
+<h1 class="percent">70%</h1>
+</div>
+<p class="name">Laravel/PHP</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section class="portfolio-area page-section scroll-to-page" id="portfolio">
+<div class="custom-container">
+<div class="portfolio-content content-width">
+<div class="section-header">
+<h4 class="subtitle scroll-animation" data-animation="fade_from_bottom">
+<i class="las la-grip-vertical"></i> portfolio
+</h4>
+<h1 class="scroll-animation" data-animation="fade_from_bottom">My<span>Projects</span></h1>
+</div>
+<div class="row portfolio-items">
+<div class="col-md-12 scroll-animation" data-animation="fade_from_bottom">
+<div class="portfolio-item portfolio-full">
+<div class="portfolio-item-inner">
+<a href="assets/images/portfolio1.png" data-lightbox="example-1">
+<img src="assets/images/portfolio1.png" alt="Portfolio">
+</a>
+<ul class="portfolio-categories">
+<li>
+<a href="#">Html Css</a>
+</li>
+<li>
+<a href="#">JavaScript</a>
+</li>
+<li>
+<a href="#">Php MySql</a>
+</li>
+</ul>
+</div>
+<h2><a href="#">E-trainerapp - webapp</a></h2>
+</div>
+</div>
+
+
+<div class="col-md-12 scroll-animation" data-animation="fade_from_bottom">
+<div class="portfolio-item portfolio-half">
+<div class="portfolio-item-inner">
+<a href="assets/images/portfolio4.jpg" data-lightbox="example-1">
+<img src="assets/images/portfolio4.jpg" alt="portfolio">
+</a>
+<ul class="portfolio-categories">
+<li>
+<a href="#">ReactJs</a>
+</li>
+<li>
+<a href="#">Scss</a>
+</li>
+</ul>
+</div>
+<h2><a href="https://nikhilchauhanyoutube.netlify.app/">Youtube Clone</a></h2>
+</div>
+</div>
+<div class="col-md-12 scroll-animation" data-animation="fade_from_bottom">
+<div class="portfolio-item portfolio-half">
+<div class="portfolio-item-inner">
+<a href="assets/images/portfolio5.jpg" data-lightbox="example-1">
+ <img src="assets/images/portfolio5.jpg" alt="portfolio">
+</a>
+<ul class="portfolio-categories">
+<li>
+<a href="#">React JS</a>
+</li>
+<li>
+<a href="#">Styled Components</a>
+</li>
+</ul>
+</div>
+<h2><a href="#">Linkedin Clone</a></h2>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+
+</div>
+</div>
+</main>
+<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/jquery.js" type="8144c0b248dad33d84b9b88a-text/javascript"></script>
+<script src="assets/js/bootstrap.bundle.min.js" type="8144c0b248dad33d84b9b88a-text/javascript"></script>
+<script src="assets/js/owl.carousel.js" type="8144c0b248dad33d84b9b88a-text/javascript"></script>
+<script src="assets/js/gsap.min.js" type="8144c0b248dad33d84b9b88a-text/javascript"></script>
+<script src="assets/js/ScrollTrigger.min.js" type="8144c0b248dad33d84b9b88a-text/javascript"></script>
+<script src="assets/js/ScrollToPlugin.min.js" type="8144c0b248dad33d84b9b88a-text/javascript"></script>
+<script src="assets/js/lightbox.min.js" type="8144c0b248dad33d84b9b88a-text/javascript"></script>
+<script src="assets/js/main.js" type="8144c0b248dad33d84b9b88a-text/javascript"></script>
+<script src="assets/js/ajax-form.js" type="8144c0b248dad33d84b9b88a-text/javascript"></script>
+<script src="assets/js/color.js" type="8144c0b248dad33d84b9b88a-text/javascript"></script>
+<script src="../../cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="8144c0b248dad33d84b9b88a-|49" defer=""></script><script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993" integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA==" data-cf-beacon='{"rayId":"7a2b81226bf929f3","version":"2023.2.0","r":1,"token":"52ad871770524b3e8ca1e53094efe67a","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
